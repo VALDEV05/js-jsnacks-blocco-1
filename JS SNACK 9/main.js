fdea54cd709f66ac1b5f888af22d2f94565335ba
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-18 18:12:37 +0200

    Commit js snack 9

--- JS SNACK 9/main.js
+++ JS SNACK 9/main.js
@@ -1,2 +1,22 @@
 /* ESERCIZIO 9
     Calcola la somma e la media dei primi 10 numeri. */
+
+
+//scrivi 10 numeri
+//calcola la somma di essi 
+//calcola la media
+
+const iteration = 10;
+let somma = 0;
+const placeToPrint = document.querySelector('.result')
+for (let index = 1; index <= iteration; index++) {
+    //somma
+    somma = somma + Number(index);
+}
+//stampa somma
+console.log(somma)
+placeToPrint.innerHTML = `La somma dei primi ${Number(iteration)} numeri è ${somma}`
+//calcolo media
+const media = somma / iteration
+console.log(media)
+placeToPrint.innerHTML += `<br>La media dei primi ${Number(iteration)} numeri è ${media}`
