b37093af24b48d8e8f15d1a93117d785fb840782
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-18 17:13:10 +0200

    Commit js snack 6

--- JS SNACK 6/main.js
+++ JS SNACK 6/main.js
@@ -1,2 +1,21 @@
 /* ESERCIZIO 6
     Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente. */
+
+
+//chiedi un numero N all'utente
+
+const numberN = Number(prompt('inserisci un numero'))
+console.log(numberN)
+const placeToPrint = document.querySelector('.result')
+let cubo;
+//se numberN == 0 --> 1
+if (numberN == 0) {
+    cubo = 1
+} else {
+    //calcolo cubo
+    cubo = numberN * numberN * numberN
+    console.log(cubo)
+
+}
+//stampa cubo del numero N
+placeToPrint.innerHTML = `Il cubo del numero ${numberN} è ${cubo}`
