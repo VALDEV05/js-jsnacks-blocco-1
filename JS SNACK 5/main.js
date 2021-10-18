5ee0b3916d0c7bd22c34f126afb6b96d8546b327
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-18 17:02:51 +0200

    Commit js snack 5

--- JS SNACK 5/main.js
+++ JS SNACK 5/main.js
@@ -1,3 +1,27 @@
 /* ESERCIZIO 5
     Crea un array vuoto.
     Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array. */
+
+//crea un arrat VUOTO
+//chiedi all'utente per 6 volte un numero
+// se dispari allora inseriscilo
+//fai il modulo di 2 se il resto è ≠ da 0 allora dispari
+//.push per aggiungere
+
+const arrayToAdd = []
+console.log(arrayToAdd);
+const placeToPrint = document.querySelector('.result')
+for (let index = 0; index < 6; index++) {
+    const element = index;
+    console.log(element);
+    const numberUser = parseInt(prompt('inserisci un numero'))
+    if ((numberUser % 2) != 0) {
+        console.log('dispari')
+        arrayToAdd.push(numberUser)
+    } else {
+        console.log('pari');
+    }
+
+}
+placeToPrint.innerHTML = arrayToAdd
+console.log(arrayToAdd);
