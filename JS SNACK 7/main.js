b8927bdc5d7c28675e549cf994c6f7a8cf748591
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-18 17:28:25 +0200

    Commit js snack 7

--- JS SNACK 7/main.js
+++ JS SNACK 7/main.js
@@ -1,2 +1,14 @@
 /* ESERCIZIO 7
     Stampa le potenze di 2 fino a 1000. */
+
+//scrivo numeri da 0 a 1000
+//calcolo la potenza
+//Math.pow(base, exponent)
+
+const placeToPrint = document.querySelector('.result')
+for (let i = 0; i <= 1000; i++) {
+    let pow = Math.pow(2, i)
+    console.log(pow)
+    let result = `<ul><li>2 alla ${i} è uguale a ${pow}</li></ul>`
+    placeToPrint.innerHTML += result;
+}
